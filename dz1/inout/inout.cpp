f3cc751fa9935eecc198f9865f3e95427cb4fb0a
ijustwanttodokursach <105460871+ijustwanttodokursach@users.noreply.github.com>
2023-02-28 20:53:51 +0300

    Add files via upload

---
+++ dz1/inout/inout.cpp
@@ -0,0 +1,13 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	int x, y;
+	cout << "x = ? "; cin >> x; 
+	cout << "y = ? "; cin >> y; 
+	cout << x << " + " << y << " = " << x + y << endl;
+	cout << x << " : " << y << " = " << x / y << endl;
+	cout << x << " * " << y << " = " << x * y << endl;
+}
+
